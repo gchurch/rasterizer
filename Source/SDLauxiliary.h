7e9f865ca1739a2803859b8fdecad5b2480709ad
George <gc14768@my.bristol.ac.uk>
2017-05-03 23:39:12 +0100

    Added function GetPixelSDL to SDLauxiliary.h which returns the rgb values of a pixel at a certain position

--- Source/SDLauxiliary.h
+++ Source/SDLauxiliary.h
@@ -79,4 +79,45 @@ void PutPixelSDL( SDL_Surface* surface, int x, int y, glm::vec3 color )
 	*p = SDL_MapRGB( surface->format, r, g, b );
 }
 
+Uint32 getpixel(SDL_Surface *surface, int x, int y)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to retrieve */
+    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch(bpp) {
+    case 1:
+        return *p;
+        break;
+
+    case 2:
+        return *(Uint16 *)p;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            return p[0] << 16 | p[1] << 8 | p[2];
+        else
+            return p[0] | p[1] << 8 | p[2] << 16;
+        break;
+
+    case 4:
+        return *(Uint32 *)p;
+        break;
+
+    default:
+        return 0;       /* shouldn't happen, but avoids warnings */
+    }
+}
+
+glm::vec3 GetPixelSDL(SDL_Surface* surface, int x, int y) {
+	Uint32 val = getpixel(surface, x, y);
+	Uint8 r;
+	Uint8 b;
+	Uint8 g;
+	SDL_GetRGB(val, surface->format, &r, &b, &g);
+	float fraction = 1.0f / 255.0f;
+	return glm::vec3(fraction * r,fraction * b,fraction * g);
+}
+
 #endif
