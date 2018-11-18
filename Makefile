7c81affc6ce0acf452ee822a8488389e2ee5bec1
George <gc14768@my.bristol.ac.uk>
2018-11-18 16:03:37 +0000

    Moved rasterizer_all.cpp to rasterizer.cpp. Also deleted redundant files

--- Makefile
+++ Makefile
@@ -1,7 +1,4 @@
 FILE=rasterizer
-CL=clipping
-TE=texture
-ALL=all
 
 ########
 #   Directories
@@ -11,13 +8,10 @@ B_DIR=build
 ########
 #   Output
 EXEC1=$(B_DIR)/$(FILE)
-EXEC2=$(B_DIR)/$(FILE)_$(CL)
-EXEC3=$(B_DIR)/$(FILE)_$(TE)
-EXEC4=$(B_DIR)/$(FILE)_$(ALL)
 
 # default build settings
-CC_OPTS=-c -pipe -Wall -Wno-switch -ggdb -g3 
-LN_OPTS=-Ofast
+CC_OPTS=-c -pipe -Wall -Wno-switch -ggdb -g3
+LN_OPTS=
 CC=g++
 
 ########
@@ -35,25 +29,12 @@ all:Build
 #   Object list
 #
 OBJ1 = $(B_DIR)/$(FILE).o
-OBJ2 = $(B_DIR)/$(FILE)_$(CL).o
-OBJ3 = $(B_DIR)/$(FILE)_$(TE).o
-OBJ4 = $(B_DIR)/$(FILE)_$(ALL).o
 
 
 ########
 #   Objects
-$(B_DIR)/$(FILE).o : $(S_DIR)/$(FILE).cpp $(S_DIR)/SDLauxiliary.h $(S_DIR)/TestModel.h
-	$(CC) $(CC_OPTS) -o $(B_DIR)/$(FILE).o $(S_DIR)/$(FILE).cpp $(SDL_CFLAGS) $(GLM_CFLAGS)
-
-$(B_DIR)/$(FILE)_$(CL).o : $(S_DIR)/$(FILE)_$(CL).cpp $(S_DIR)/SDLauxiliary.h $(S_DIR)/TestModel.h
-	$(CC) $(CC_OPTS) -o $(B_DIR)/$(FILE)_$(CL).o $(S_DIR)/$(FILE)_$(CL).cpp $(SDL_CFLAGS) $(GLM_CFLAGS)
-
-$(B_DIR)/$(FILE)_$(TE).o : $(S_DIR)/$(FILE)_$(TE).cpp $(S_DIR)/SDLauxiliary.h $(S_DIR)/TestModel_texture.h
-	$(CC) $(CC_OPTS) -o $(B_DIR)/$(FILE)_$(TE).o $(S_DIR)/$(FILE)_$(TE).cpp $(SDL_CFLAGS) $(GLM_CFLAGS)
-
-$(B_DIR)/$(FILE)_$(ALL).o : $(S_DIR)/$(FILE)_$(ALL).cpp $(S_DIR)/SDLauxiliary.h $(S_DIR)/TestModel_texture.h
-	$(CC) $(CC_OPTS) -o $(B_DIR)/$(FILE)_$(ALL).o $(S_DIR)/$(FILE)_$(ALL).cpp $(SDL_CFLAGS) $(GLM_CFLAGS)
-
+$(OBJ1) : $(S_DIR)/$(FILE).cpp $(S_DIR)/SDLauxiliary.h $(S_DIR)/TestModel.h
+	$(CC) $(CC_OPTS) $(S_DIR)/$(FILE).cpp -o $(OBJ1) $(SDL_CFLAGS) $(GLM_CFLAGS)
 
 
 ########
@@ -61,19 +42,8 @@ $(B_DIR)/$(FILE)_$(ALL).o : $(S_DIR)/$(FILE)_$(ALL).cpp $(S_DIR)/SDLauxiliary.h 
 Build : $(OBJ1) Makefile
 	$(CC) $(LN_OPTS) -o $(EXEC1) $(OBJ1) $(SDL_LDFLAGS)
 
-clipping : $(OBJ2) Makefile
-	$(CC) $(LN_OPTS) -o $(EXEC2) $(OBJ2) $(SDL_LDFLAGS)
-
-texture : $(OBJ3) Makefile
-	$(CC) $(LN_OPTS) -o $(EXEC3) $(OBJ3) $(SDL_LDFLAGS)
-
-all : $(OBJ4) Makefile
-	$(CC) $(LN_OPTS) -o $(EXEC4) $(OBJ4) $(SDL_LDFLAGS)
-
-run : $(EXEC4)
-	./$(EXEC4)
-
-
+run : $(EXEC1)
+	./$(EXEC1)
 
 clean:
 	rm -f $(B_DIR)/* 
--- src/TestModel.h
+++ src/TestModel.h
@@ -6,26 +6,40 @@
 #include <glm/glm.hpp>
 #include <vector>
 
+class V
+{
+public:
+	glm::vec3 pos3d;
+	glm::ivec2 textureCoordinates;
+
+	V(glm::vec3 pos3d, glm::ivec2 textureCoordinates)
+		: pos3d(pos3d), textureCoordinates(textureCoordinates)
+	{
+
+	}
+};
+
 // Used to describe a triangular surface:
 class Triangle
 {
 public:
-	glm::vec3 v0;
-	glm::vec3 v1;
-	glm::vec3 v2;
+	V v0;
+	V v1;
+	V v2;
 	glm::vec3 normal;
 	glm::vec3 color;
+	int texture;
 
-	Triangle( glm::vec3 v0, glm::vec3 v1, glm::vec3 v2, glm::vec3 color )
-		: v0(v0), v1(v1), v2(v2), color(color)
+	Triangle( V v0, V v1, V v2, glm::vec3 color, int texture )
+		: v0(v0), v1(v1), v2(v2), color(color), texture(texture)
 	{
 		ComputeNormal();
 	}
 
 	void ComputeNormal()
 	{
-		glm::vec3 e1 = v1-v0;
-		glm::vec3 e2 = v2-v0;
+		glm::vec3 e1 = v1.pos3d-v0.pos3d;
+		glm::vec3 e2 = v2.pos3d-v0.pos3d;
 		normal = glm::normalize( glm::cross( e2, e1 ) );
 	}
 };
@@ -37,6 +51,7 @@ public:
 void LoadTestModel( std::vector<Triangle>& triangles )
 {
 	using glm::vec3;
+	using glm::ivec2;
 
 	// Defines colors:
 	vec3 red(    0.75f, 0.15f, 0.15f );
@@ -55,101 +70,102 @@ void LoadTestModel( std::vector<Triangle>& triangles )
 
 	float L = 555;			// Length of Cornell Box side.
 
-	vec3 A(L,0,0);
-	vec3 B(0,0,0);
-	vec3 C(L,0,L);
-	vec3 D(0,0,L);
+	V A(vec3(L,0,0), ivec2(511,511));
+	V B(vec3(0,0,0), ivec2(0,511));
+	V C(vec3(L,0,L), ivec2(511,0));
+	V D(vec3(0,0,L), ivec2(0,0));
 
-	vec3 E(L,L,0);
-	vec3 F(0,L,0);
-	vec3 G(L,L,L);
-	vec3 H(0,L,L);
+	V E(vec3(L,L,0), ivec2(0,0));
+	V F(vec3(0,L,0), ivec2(0,0));
+	V G(vec3(L,L,L), ivec2(0,0));
+	V H(vec3(0,L,L), ivec2(0,0));
+
 
 	// Floor:
-	triangles.push_back( Triangle( C, B, A, green ) );
-	triangles.push_back( Triangle( C, D, B, green ) );
+	triangles.push_back( Triangle( C, B, A, green, 1 ) );
+	triangles.push_back( Triangle( C, D, B, green, 1 ) );
 
 	// Left wall
-	triangles.push_back( Triangle( A, E, C, purple ) );
-	triangles.push_back( Triangle( C, E, G, purple ) );
+	triangles.push_back( Triangle( A, E, C, purple, 0 ) );
+	triangles.push_back( Triangle( C, E, G, purple, 0 ) );
 
 	// Right wall
-	triangles.push_back( Triangle( F, B, D, yellow ) );
-	triangles.push_back( Triangle( H, F, D, yellow ) );
+	triangles.push_back( Triangle( F, B, D, yellow, 0 ) );
+	triangles.push_back( Triangle( H, F, D, yellow, 0 ) );
 
 	// Ceiling
-	triangles.push_back( Triangle( E, F, G, cyan ) );
-	triangles.push_back( Triangle( F, H, G, cyan ) );
+	triangles.push_back( Triangle( E, F, G, cyan, 0 ) );
+	triangles.push_back( Triangle( F, H, G, cyan, 0 ) );
 
 	// Back wall
-	triangles.push_back( Triangle( G, D, C, white ) );
-	triangles.push_back( Triangle( G, H, D, white ) );
+	triangles.push_back( Triangle( G, D, C, white, 0 ) );
+	triangles.push_back( Triangle( G, H, D, white, 0 ) );
 
 	// ---------------------------------------------------------------------------
 	// Short block
 
-	A = vec3(290,0,114);
-	B = vec3(130,0, 65);
-	C = vec3(240,0,272);
-	D = vec3( 82,0,225);
+	A = V(vec3(290,0,114), ivec2(0,0));
+	B = V(vec3(130,0, 65), ivec2(0,0));
+	C = V(vec3(240,0,272), ivec2(0,0));
+	D = V(vec3( 82,0,225), ivec2(0,0));
 
-	E = vec3(290,165,114);
-	F = vec3(130,165, 65);
-	G = vec3(240,165,272);
-	H = vec3( 82,165,225);
-
-	// Front
-	triangles.push_back( Triangle(E,B,A,red) );
-	triangles.push_back( Triangle(E,F,B,red) );
+	E = V(vec3(290,165,114), ivec2(0,0));
+	F = V(vec3(130,165, 65), ivec2(0,0));
+	G = V(vec3(240,165,272), ivec2(0,0));
+	H = V(vec3( 82,165,225), ivec2(0,0));
 
 	// Front
-	triangles.push_back( Triangle(F,D,B,red) );
-	triangles.push_back( Triangle(F,H,D,red) );
+	triangles.push_back( Triangle(E,B,A,red,0) );
+	triangles.push_back( Triangle(E,F,B,red,0) );
+
+	// Front
+	triangles.push_back( Triangle(F,D,B,red,0) );
+	triangles.push_back( Triangle(F,H,D,red,0) );
 
 	// BACK
-	triangles.push_back( Triangle(H,C,D,red) );
-	triangles.push_back( Triangle(H,G,C,red) );
+	triangles.push_back( Triangle(H,C,D,red,0) );
+	triangles.push_back( Triangle(H,G,C,red,0) );
 
 	// LEFT
-	triangles.push_back( Triangle(G,E,C,red) );
-	triangles.push_back( Triangle(E,A,C,red) );
+	triangles.push_back( Triangle(G,E,C,red,0) );
+	triangles.push_back( Triangle(E,A,C,red,0) );
 
 	// TOP
-	triangles.push_back( Triangle(G,F,E,red) );
-	triangles.push_back( Triangle(G,H,F,red) );
+	triangles.push_back( Triangle(G,F,E,red,0) );
+	triangles.push_back( Triangle(G,H,F,red,0) );
 
 	// ---------------------------------------------------------------------------
 	// Tall block
 
-	A = vec3(423,0,247);
-	B = vec3(265,0,296);
-	C = vec3(472,0,406);
-	D = vec3(314,0,456);
+	A = V(vec3(423,0,247), ivec2(0,0));
+	B = V(vec3(265,0,296), ivec2(0,0));
+	C = V(vec3(472,0,406), ivec2(0,0));
+	D = V(vec3(314,0,456), ivec2(0,0));
 
-	E = vec3(423,330,247);
-	F = vec3(265,330,296);
-	G = vec3(472,330,406);
-	H = vec3(314,330,456);
-
-	// Front
-	triangles.push_back( Triangle(E,B,A,blue) );
-	triangles.push_back( Triangle(E,F,B,blue) );
+	E = V(vec3(423,330,247), ivec2(0,0));
+	F = V(vec3(265,330,296), ivec2(0,0));
+	G = V(vec3(472,330,406), ivec2(0,0));
+	H = V(vec3(314,330,456), ivec2(0,0));
 
 	// Front
-	triangles.push_back( Triangle(F,D,B,blue) );
-	triangles.push_back( Triangle(F,H,D,blue) );
+	triangles.push_back( Triangle(E,B,A,blue,0) );
+	triangles.push_back( Triangle(E,F,B,blue,0) );
+
+	// Front
+	triangles.push_back( Triangle(F,D,B,blue,0) );
+	triangles.push_back( Triangle(F,H,D,blue,0) );
 
 	// BACK
-	triangles.push_back( Triangle(H,C,D,blue) );
-	triangles.push_back( Triangle(H,G,C,blue) );
+	triangles.push_back( Triangle(H,C,D,blue,0) );
+	triangles.push_back( Triangle(H,G,C,blue,0) );
 
 	// LEFT
-	triangles.push_back( Triangle(G,E,C,blue) );
-	triangles.push_back( Triangle(E,A,C,blue) );
+	triangles.push_back( Triangle(G,E,C,blue,0) );
+	triangles.push_back( Triangle(E,A,C,blue,0) );
 
 	// TOP
-	triangles.push_back( Triangle(G,F,E,blue) );
-	triangles.push_back( Triangle(G,H,F,blue) );
+	triangles.push_back( Triangle(G,F,E,blue,0) );
+	triangles.push_back( Triangle(G,H,F,blue,0) );
 
 
 	// ----------------------------------------------
@@ -157,21 +173,21 @@ void LoadTestModel( std::vector<Triangle>& triangles )
 
 	for( size_t i=0; i<triangles.size(); ++i )
 	{
-		triangles[i].v0 *= 2/L;
-		triangles[i].v1 *= 2/L;
-		triangles[i].v2 *= 2/L;
+		triangles[i].v0.pos3d *= 2/L;
+		triangles[i].v1.pos3d *= 2/L;
+		triangles[i].v2.pos3d *= 2/L;
 
-		triangles[i].v0 -= vec3(1,1,1);
-		triangles[i].v1 -= vec3(1,1,1);
-		triangles[i].v2 -= vec3(1,1,1);
+		triangles[i].v0.pos3d -= vec3(1,1,1);
+		triangles[i].v1.pos3d -= vec3(1,1,1);
+		triangles[i].v2.pos3d -= vec3(1,1,1);
 
-		triangles[i].v0.x *= -1;
-		triangles[i].v1.x *= -1;
-		triangles[i].v2.x *= -1;
+		triangles[i].v0.pos3d.x *= -1;
+		triangles[i].v1.pos3d.x *= -1;
+		triangles[i].v2.pos3d.x *= -1;
 
-		triangles[i].v0.y *= -1;
-		triangles[i].v1.y *= -1;
-		triangles[i].v2.y *= -1;
+		triangles[i].v0.pos3d.y *= -1;
+		triangles[i].v1.pos3d.y *= -1;
+		triangles[i].v2.pos3d.y *= -1;
 
 		triangles[i].ComputeNormal();
 	}
